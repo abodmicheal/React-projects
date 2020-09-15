86f0b58fb0fa5e8cf9d34a8d2407632ea496569c
Abod <56658666+abodmicheal@users.noreply.github.com>
2020-09-15 19:02:36 +0100

    Add files via upload

---
+++ Netflix UI Web App/src/Movieslist.js
@@ -0,0 +1,17 @@
+const Movieslist = {
+  trending: "/trending/all/week?api_key=b847523692611caaffd06d42f9bd0ea9",
+  netflixOriginals:
+    "/discover/tv?api_key=b847523692611caaffd06d42f9bd0ea9&page=1&with_networks=213",
+  topRated: "/movie/top_rated?api_key=b847523692611caaffd06d42f9bd0ea9&page=2",
+  actionMovies:
+    "/discover/movie?api_key=b847523692611caaffd06d42f9bd0ea9&page=1&with_genres=28",
+  comedyMovies:
+    "/discover/movie?api_key=b847523692611caaffd06d42f9bd0ea9&page=1&with_genres=35",
+  horrorMovies:
+    "/discover/movie?api_key=b847523692611caaffd06d42f9bd0ea9&page=1&with_genres=27",
+  romanceMovies:
+    "/discover/movie?api_key=b847523692611caaffd06d42f9bd0ea9&page=1&with_genres=10749",
+  documentary:
+    "/discover/movie?api_key=b847523692611caaffd06d42f9bd0ea9&page=1&with_genres=99",
+};
+export default Movieslist;
